cdaada88013a586c5c1a0b84cdba7cbeec8dab42
Ahmed <1615993@students.wits.ac.za>
2021-03-25 11:49:26 +0200

    Added Routes

---
+++ index.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const mainRouter = require('./mainRoutes')
+
+app.use(mainRouter);
+app.listen(3000);
+console.log("Express server running on port 3000");
